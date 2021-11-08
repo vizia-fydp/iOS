9c223179131225437661909c30b1c55c9f6690f6
connorbarkr <connorbarkr@gmail.com>
2021-11-08 14:35:21 -0500

    Added action button pagination

--- Project Iris/Project Iris/ContentView.swift
+++ Project Iris/Project Iris/ContentView.swift
@@ -8,15 +8,36 @@
 import SwiftUI
 
 struct PrimaryButton: ButtonStyle {
+    @Binding var currentButton: Int
+    var totalButtons: Int
+    
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(
                 ZStack {
                     Color("appWhite")
-                    HStack(spacing: 225) {
-                        Image(systemName: "chevron.left")
-                        Image(systemName: "chevron.right")
+                    HStack(spacing: 180) {
+                        Button {
+                            if ($currentButton.wrappedValue == 1) {
+                                self.currentButton = self.totalButtons
+                            } else {
+                                self.currentButton = self.currentButton - 1
+                            }
+                        } label: {
+                            Image(systemName: "chevron.left")
+                        }
+                        .buttonStyle(ChevronButton())
+                        Button {
+                            if ($currentButton.wrappedValue == self.totalButtons) {
+                                self.currentButton = 1
+                            } else {
+                                self.currentButton = self.currentButton + 1
+                            }
+                        } label: {
+                            Image(systemName: "chevron.right")
+                        }
+                        .buttonStyle(ChevronButton())
                     }
                 }
             )
@@ -41,20 +62,28 @@ struct PageButton: ButtonStyle {
     }
 }
 
+struct ChevronButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(.horizontal, 30)
+            .padding(.vertical, 30)
+    }
+}
+
 struct AppThemeContainer <Content : View> : View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-    
+
     var content : Content
     var pageTitle : String
     var home : Bool
-    
+
     init(pageTitle: String, home: Bool, @ViewBuilder content: () -> Content) {
         self.content = content()
         self.pageTitle = pageTitle
         self.home = home
         UINavigationBar.appearance().backgroundColor = UIColor(Color("appWhite"))
     }
-    
+
     var body: some View {
         ZStack {
             Color("appWhite")
@@ -84,7 +113,7 @@ struct AppThemeContainer <Content : View> : View {
                 }
                 .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 70)
                 .padding(.bottom, 15)
-                
+
                 VStack {
                     content
                 }
@@ -96,17 +125,21 @@ struct AppThemeContainer <Content : View> : View {
 }
 
 struct HomeView: View {
+    @State private var currentButton: Int = 1
+    private var actionButtons: [Int:String] = [
+        1:"scan\ntext",
+        2:"detect\nbill",
+        3:"detect\ncolor"
+    ]
+    
     var body: some View {
         AppThemeContainer(pageTitle: "home", home: true) {
-            NavigationLink(destination: PlaybackView()) {
-                Text("detect\ncolor")
-            }
-            .buttonStyle(PrimaryButton())
-            
+            ButtonCarouselView(buttons: actionButtons, currentButton: $currentButton)
+
             Button {
-                print("page 2")
+                currentButton = currentButton == actionButtons.count ? 1 : currentButton + 1
             } label: {
-                Text("2/3")
+                Text("\($currentButton.wrappedValue)/\(actionButtons.count)")
             }
             .buttonStyle(PageButton())
         }
@@ -118,6 +151,32 @@ struct PlaybackView: View {
         AppThemeContainer(pageTitle: "playback", home: false) {
             Text("hiiii lol")
         }
+    }
+}
+
+struct ButtonCarouselView: View {
+    @Binding var currentButton: Int
+    var buttons : [Int: String]
+    
+    init(buttons: [Int: String], currentButton: Binding<Int>) {
+        self._currentButton = currentButton
+        self.buttons = buttons
+        UIPageControl.appearance().currentPageIndicatorTintColor = UIColor(Color("appBlack"))
+        UIPageControl.appearance().pageIndicatorTintColor = UIColor.black.withAlphaComponent(0.3)
+    }
+    
+    var body: some View {
+        TabView(selection: self.$currentButton) {
+            ForEach(1..<(self.buttons.count + 1)) { i in
+                NavigationLink(destination: PlaybackView()) {
+                    Text(self.buttons[i] ?? "default")
+                }
+                .buttonStyle(PrimaryButton(currentButton: $currentButton, totalButtons: self.buttons.count))
+                .tag(i)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .tabViewStyle(PageTabViewStyle())
     }
 }
 
